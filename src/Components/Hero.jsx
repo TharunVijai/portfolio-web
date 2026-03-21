29c742817c56d56d1e95b8071f58dc62a02ab524
Tharun Vijay S <tharunrust@gmail.com>
2026-03-21 13:50:12 +0530

    added button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,6 +10,7 @@ const Hero = () => {
                 <p className='p2'>Java Full Stack Developer</p><br />
                 <p>An aspiring Full Stack Java Developer with a strong foundation in Java (OOPS)   and web technologies. 
                   Passionate about creating efficient, scalable solutions and continuously learning new technologies.”</p>
+                <a href="Resume.pdf" target='' className='button-common'> See Resume</a>
             </div>
             <div className='heroPhoto'>
               <h1>photo</h1>
